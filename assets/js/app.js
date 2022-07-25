0b3533a403af3281dce0f4f6163ecad788c0d22c
maluy <artem.evsiukov1@gmail.com>
2022-07-25 13:35:13 +0300

    add

---
+++ assets/js/app.js
@@ -0,0 +1,71 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+var _burgerMenu = _interopRequireDefault(require("./components/burger-menu"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// You can write a call and import your functions in this file.
+//
+// This file will be compiled into app.js and will not be minified.
+// Feel free with using ES6 here.
+//import Accordion from './components/accordion';
+(function ($) {
+  // When DOM is ready
+  $(function () {
+    //const accordions = new Accordion();
+    _burgerMenu["default"].init();
+  });
+})(jQuery);
+
+},{"./components/burger-menu":2}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var BURGER = document.querySelector('.js-burger-open');
+var NAV = document.querySelector('.js-burger');
+var BODY = document.querySelector('body');
+var CLASS_OVERFLOW = 'overflow';
+var CLASS_ACTIVE = 'active';
+
+var burgerMenu = function () {
+  var burgeInit = function burgeInit() {
+    if (!BURGER) return;
+    BURGER.addEventListener('click', function (e) {
+      if (!e.currentTarget.classList.contains('active')) {
+        openBurger();
+      } else {
+        closeBurger();
+      }
+    });
+  };
+
+  var openBurger = function openBurger() {
+    BURGER.classList.add(CLASS_ACTIVE);
+    NAV.classList.add(CLASS_ACTIVE);
+    BODY.classList.add(CLASS_OVERFLOW);
+  };
+
+  var closeBurger = function closeBurger() {
+    BURGER.classList.remove(CLASS_ACTIVE);
+    NAV.classList.remove(CLASS_ACTIVE);
+    BODY.classList.remove(CLASS_OVERFLOW);
+  };
+
+  var init = function init() {
+    burgeInit();
+  };
+
+  return {
+    init: init,
+    closeBurger: closeBurger
+  };
+}();
+
+var _default = burgerMenu;
+exports["default"] = _default;
+
+},{}]},{},[1]);
